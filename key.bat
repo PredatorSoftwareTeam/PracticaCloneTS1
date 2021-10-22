5033c548bebf53b918d41c35012daff93ab13545
Omar_Andres_Ibarra_Olivas <l18330450@hermosilo.tecnm.mx>
2021-10-21 20:05:55 -0700

    Commit desde consola

---
+++ key.bat
@@ -0,0 +1,9 @@
+@echo off
+echo.
+echo Hasta este momento no tengo problemas con lo visto en clase
+echo solo tuve pequeñas dudas en algunos puntos de esta actividad
+echo que tal vez no realice bien pero se pueden solucionar.
+echo.
+pause
+exit
+EOF
